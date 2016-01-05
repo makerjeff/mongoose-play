1a2ac446c783f275229a60a1e95760ff7316ea05
Jeff Wu <makerjeffwu@gmail.com>
2016-01-05 10:17:39 -0800

    server working, now moving on to mongoose_site.js

---
+++ mongoose_site.js
@@ -0,0 +1,7 @@
+/**
+ * Created by jeffersonwu on 1/4/16.
+ */
+
+//MONGOOSEJS.COM Getting Started
+
+
--- server.js
+++ server.js
@@ -1,1 +1,44 @@
 // = MONGOOSE PLAY SERVER.JS =
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/myTestDB');
+
+var db = mongoose.connection;
+
+db.on('error', function(err){
+    console.log('connection error', err);
+});
+
+db.once('open', function(){
+    console.log('connected!');
+});
+
+// == create SCHEMA ==
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name: String,
+    age: Number,
+    DOB: Date,
+    isAlive: Boolean
+});
+
+// == create MODEL ==
+var User = mongoose.model('User', userSchema);
+
+// == user instances ==
+var arvind = new User({
+    name:'Arvind',
+    age: 99,
+    DOB: '01/01/1915',
+    isAlive: true
+});
+
+arvind.save(function(err, data){
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log('Saved: ', data);
+    }
+});
