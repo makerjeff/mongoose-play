3a891b47db5d83d9bdb4d43d7b897d5ed701ad37
Jeff Wu <makerjeffwu@gmail.com>
2016-01-06 09:54:23 -0800

    latest from after-game work at home

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Experimenting with Mongoose.
 Tutorials to trudge through:
 http://blog.mongodirector.com/getting-started-with-mongodb-and-mongoose/
 http://blog.modulus.io/getting-started-with-mongoose
+
+Basic Workflow:
+- include 'mongoose' module
+- create connection
+- define connection error handler
+- open DB connection using 'once' method
+- create schema
+- create model
--- mongoose_site.js
+++ mongoose_site.js
@@ -2,6 +2,27 @@
  * Created by jeffersonwu on 1/4/16.
  */
 
-//MONGOOSEJS.COM Getting Started
+//MONGOOSEJS.COM Getting Started:
+// link: http://mongoosejs.com/docs/index.html
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+//error handling
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//open mongoose connection
+db.once('open', function(){
+    console.log('we are connected!');
+});
+
+//create schema
+var kittySchema = mongoose.schema({
+    name: String
+});
+
+//create model
+var Kitten = mongoose.model('Kitten', kittySchema);
 
 
