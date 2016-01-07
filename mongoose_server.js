d9c79c8ee350d6d8d20521f0af9034499a9ee3fe
Jefferson Wu <makerjeffwu@gmail.com>
2016-01-06 18:44:46 -0800

    updated mongoose_server.js to express + mongoose, created basic index.html and a basic 404.html

---
+++ mongoose_server.js
@@ -0,0 +1,81 @@
+/**
+ * Created by jefferson.wu on 1/6/16.
+ */
+
+var express = require('express');
+var colors = require('colors');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// ===================
+// ===== EXPRESS =====
+// ===================
+
+var app = express();
+var port = process.argv[2];
+
+//serve static files
+app.use(express.static(__dirname + '/public'));
+
+//parse JSON middleware
+app.use(bodyParser.json());
+
+//custom console logging middleware - auto logs
+app.use(function(request, response, next) {
+    console.log('method: %s, url: %s, path: %s', request.method, request.url, request.path);
+    next();
+});
+
+// == CUSTOM ROUTES ==
+//basic 404
+
+app.get('/public/*', function(request, response){
+    response.type('text/plain');
+    response.sendFile(__dirname + '/public/404.html');
+});
+
+// ====================
+// ===== MONGOOSE =====
+// ====================
+
+//create connection
+mongoose.connect('mongodb://localhost/test');
+
+//put handle on connection
+var db = mongoose.connection;
+
+//error handling
+db.on('error', function(err0r){
+    console.error('connection error: %s', err0r);
+});
+
+//start the server
+db.once('open', function(){
+    console.log('MongoDB connection established.'.green);
+});
+
+//define schema
+
+//define schema methods
+
+//define model
+
+//add starter data (optional)
+//save starter data (optional)
+
+//=====================
+//===== MAIN CODE =====
+//=====================
+
+//start the server
+init();
+
+
+
+//============================
+//===== CUSTOM FUNCTIONS =====
+//============================
+
+function init(){
+    console.log(colors.green('Starting server on ' + port + ', running on a ' + process.arch + 'machine. '));
+}
